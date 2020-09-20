288f6967b7d3e00af37e4c24e99cc93b6b92eda0
Gerson Toribio <gerson.toribio@pucp.pe>
2020-09-20 01:14:34 -0500

    Adding build file

---
+++ build/precache-manifest.88a8a05d8692078ab193fb8c4202f1cb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebbd1bfa7dc2d901c15041d9bde7774f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "676af20ae85ab5697b6b",
+    "url": "./static/css/main.7014d356.chunk.css"
+  },
+  {
+    "revision": "8d96399d3aadb206347f",
+    "url": "./static/js/2.0bf33bd9.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.0bf33bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676af20ae85ab5697b6b",
+    "url": "./static/js/main.f1ee509c.chunk.js"
+  },
+  {
+    "revision": "a8af0eba501c5e015c11",
+    "url": "./static/js/runtime-main.8db3bbf6.js"
+  },
+  {
+    "revision": "545dede700bc62a7f50376bf63061bca",
+    "url": "./static/media/arrow-right.545dede7.svg"
+  },
+  {
+    "revision": "facbfb5fd5ccc75dcef1ae2a954c29ff",
+    "url": "./static/media/background.facbfb5f.jpg"
+  },
+  {
+    "revision": "6e85432f616e96a4443f83aaed1302c3",
+    "url": "./static/media/image_opacity.6e85432f.png"
+  },
+  {
+    "revision": "2dddf6f04e3ee78c2347fb4303fd6f6e",
+    "url": "./static/media/regular_clock.2dddf6f0.svg"
+  },
+  {
+    "revision": "987e802d53fe6535bca3465b6fec1a7a",
+    "url": "./static/media/zoovu-o.987e802d.svg"
+  },
+  {
+    "revision": "509452d28bc679f17bb2aa688349e96c",
+    "url": "./static/media/zoovu-u.509452d2.svg"
+  },
+  {
+    "revision": "f04e5e7ec4ba17efc272641f25e9fa3e",
+    "url": "./static/media/zoovu-v.f04e5e7e.svg"
+  },
+  {
+    "revision": "98d275b2a558436f42b522be4a58e71d",
+    "url": "./static/media/zoovu-z.98d275b2.svg"
+  }
+]);
